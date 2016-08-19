00f554350d2882b7cd5b6379769ce3d20b0c6955
Patrick Lewis <patrickml.pml@gmail.com>
2016-08-18 22:15:14 -0400

    #6 create route

---
+++ webapp/votify/client/routes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import mount from 'react-mounter';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+// home page
+FlowRouter.route('/', {
+  name: 'home',
+  action() {
+
+  }
+})
