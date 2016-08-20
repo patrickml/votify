3cc0d135c71c63d5ab5ff5de2cddd7902ff5ca35
Patrick Lewis <patrickml.pml@gmail.com>
2016-08-19 22:54:23 -0400

    #8 Fixed Comments: Added Docs and Alt Tags

--- webapp/votify/client/imports/components/ui/track/track.js
+++ webapp/votify/client/imports/components/ui/track/track.js
@@ -1,15 +1,25 @@
 import React, { PropTypes } from 'react';
 
+/**
+ * Creates a list of artists from the array given from spotify
+ * @method getArtists
+ * @param  {Array}   artists the artists for the traack
+ * @return {String}  the artists
+ */
 const getArtists = (artists) => artists.map((artist) => artist.name).join(' & ');
 
 const Track = ({ track: { album, name, artists } }) => (
   <div className="track">
-    <img src={album.images[0].url} className="album" />
+    <img src={album.images[0].url} className="album" alt="album cover" />
     <ul className="details">
       <li className="name">{name}</li>
       <li className="artist">by {getArtists(artists)}</li>
     </ul>
   </div>
 );
+
+Track.propTypes = {
+  track: PropTypes.object,
+};
 
 export default Track;
