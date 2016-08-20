393a1a1fec9af16609386e57ec19fa48539678b1
Patrick Lewis <patrickml.pml@gmail.com>
2016-08-19 21:24:39 -0400

    # added docs

--- both/imports/util/get.http.js
+++ both/imports/util/get.http.js
@@ -1,5 +1,11 @@
 import { HTTP } from 'meteor/http';
 
+/**
+ * Async get http request
+ * @param   {String} url    the url to call
+ * @param   {Object} params the params to use for the call
+ * @return  {Promise}
+ */
 export default (url, params) => (
   new Promise((resolve, reject) => {
     HTTP.get(url, params, (err, res) => {
