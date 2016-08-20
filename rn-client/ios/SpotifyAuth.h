150928fad28eb82c0e58e284249f3f8a53bea2d2
JD Angerhofer <jd@angerhofer.co>
2016-08-20 12:16:08 -0400

    Basic Spotify Login Flow

---
+++ rn-client/ios/SpotifyAuth.h
@@ -0,0 +1,16 @@
+//
+//  SpotifyAuth.h
+//  spotifyModule
+//
+//  Created by Jack on 8/8/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+
+@interface SpotifyAuth : NSObject <RCTBridgeModule>
+@property (nonatomic, strong) NSString *myScheme;
+-(void)urlCallback: (NSURL *)url;
++ (id)sharedManager;
+@end
