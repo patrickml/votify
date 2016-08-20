6d8174d41ab2834708827d3e1b1d0f979bd76aa7
Patrick Lewis <patrickml.pml@gmail.com>
2016-08-20 14:12:44 -0400

    #12 added docs

--- webapp/votify/client/imports/components/ui/track/cover.js
+++ webapp/votify/client/imports/components/ui/track/cover.js
@@ -3,6 +3,12 @@ import { composeWithTracker } from 'react-komposer';
 import EventHorizon from 'meteor/patrickml:event-horizon';
 import { playTrack, stopPlaying } from '../../../actions/player.actions';
 
+/**
+ * Returns a style object with a the cover background image
+ * @method getBackground
+ * @param  {String}      url the url of the cover background image
+ * @return {Object}
+ */
 const getBackground = (url) => (
   {
     background: `url(${url}) no-repeat center center/cover`,
@@ -20,6 +26,12 @@ const Cover = ({ url, playing, previewUrl }) => (
     }
   </div>
 );
+
+Cover.propTypes = {
+  url: PropTypes.string,
+  playing: PropTypes.string,
+  previewUrl: PropTypes.string,
+};
 
 export default composeWithTracker((props, onData) => {
   const { src, playing } = EventHorizon.subscribe('player');
