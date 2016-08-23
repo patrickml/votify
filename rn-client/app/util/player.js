fcc25553c5a7c06b085ecbad17461ce12b100340
Patrick Lewis <patrickml.pml@gmail.com>
2016-08-22 23:44:03 -0400

    #50 Added Fading

--- rn-client/app/util/player.js
+++ rn-client/app/util/player.js
@@ -5,15 +5,55 @@ import Meteor from 'react-native-meteor';
 import { setPlayingStatus } from '../actions/player.actions';
 const { SpotifyAuth } = NativeModules;
 
+let fading = false;
+
+/**
+ * Fade music either in or out
+ */
+export const fade = (fadeIn = true, callback = () => {}, callbackFirst = false) => {
+  // make sure we are not currently fading
+  if (!fading) {
+    // check if we need to call the callback first
+    if (callbackFirst) {
+      callback();
+    }
+    // set the global fading variable
+    fading = true;
+    // start our volume depending on fading in or out
+    let volume = fadeIn ? 0 : 1;
+    const interval = setInterval(() => {
+      // increment or decrement our volume
+      volume = volume + (0.01 * (fadeIn ? 1 : -1));
+      // set the spotify volme
+      SpotifyAuth.setVolume(volume, () => {});
+      // once finished fading clear the interval and call the callback if needed
+      if ((fadeIn && volume >= 1) || (!fadeIn && volume <= 0.01)) {
+        // clear the interval
+        clearInterval(interval);
+        // set the global fading variable to false
+        fading = false;
+        // call the callback if we didn't do it at the begining
+        if (!callbackFirst) {
+          callback();
+        }
+      }
+    }, 10);
+  }
+};
+
 /**
  * Toggles the current song to play or pause
  * @method togglePlayStatus
  * @param  {Boolean} status the play or not to play
  */
 export const togglePlayStatus = (status) => {
-  SpotifyAuth.setIsPlaying(status, () => {
-    setPlayingStatus(status);
-  });
+  // set the status of the player
+  setPlayingStatus(status);
+  // fade the music then change the status of the player with spotify
+  fade(status, () => {
+    // change the spotify player status
+    SpotifyAuth.setIsPlaying(status, () => {});
+  }, status);
 };
 
 /**
@@ -21,6 +61,7 @@ export const togglePlayStatus = (status) => {
  * @method removeSong
  */
 export const removeSong = () => {
+  // find the top song
   const track = Meteor.collection('tracks').findOne({}, {
     sort: {
       playing: -1,
@@ -28,5 +69,6 @@ export const removeSong = () => {
       createdAt: 1,
     },
   });
+  // remove the top song
   Meteor.collection('tracks').remove(track._id);
 };
