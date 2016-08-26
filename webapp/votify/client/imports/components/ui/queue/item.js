6795a425928ea79ed4b969ccb856fdefdf07367b
Patrick Lewis <patrickml.pml@gmail.com>
2016-08-26 09:52:56 -0400

    Remove Background Styles

--- webapp/votify/client/imports/components/ui/queue/item.js
+++ webapp/votify/client/imports/components/ui/queue/item.js
@@ -3,13 +3,8 @@ import Track from '../track/track';
 import Like from './like';
 import Add from './add';
 
-const getBackground = (url) => (
-  { background: `url(${url}) no-repeat center center fixed` }
-);
-
 const Item = ({ track, search }) => (
   <li className="queue-item">
-    <div className="background" style={getBackground(_.get(track, 'album.images.0.url'))} />
     <Track track={track} />
     {
       search && <Add track={track} /> || <Like track={track} />
